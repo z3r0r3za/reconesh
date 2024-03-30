1ebfee5c42fae1cce4803f3959c9ec6223dbe329
Pierce Nylon <piercenylon@gmail.com>
2024-03-30 16:01:16 -0700

    Add dig for ip, nmap for open ports, validate domain, change variable names, count some line numbers one at a time until satisfied.

--- recon.sh
+++ recon.sh
@@ -15,28 +15,46 @@ GREEN="\033[1;32m"
 RESET="\033[0m"
 # Set up directories.
 base_dir="${domain}_$(date +'%Y%m%dT%H%M%S')"
-info_path="$base_dir/info"
-subdomain_path="$base_dir/subdomains"
-screenshot_path="$base_dir/screenshots"
+nmap="$base_dir/nmap"
+information="$base_dir/info"
+subdomains="$base_dir/subdomains"
+screenshots="$base_dir/screenshots"
+validate_domain="^([a-zA-Z0-9][a-zA-Z0-9-]{0,61}[a-zA-Z0-9]\.)+[a-zA-Z]{2,}$"
 
-# Create directories.
-for path in "$info_path" "$subdomain_path" "$screenshot_path"; do
-    if [ ! -d "$path" ]; then
-        mkdir -p "$path"
-        echo "Creating directory: $path"
-    fi
-done
+# Check if domain is valid.
+if [[ "$domain" =~ $validate_domain ]]; then
+    # Create directories for domain.
+    echo "Creating directories..."
+    for path in "$nmap" "$information" "$subdomains" "$screenshots"; do
+        if [ ! -d "$path" ]; then
+            mkdir -p "$path"
+            echo "$path"
+        fi
+    done
+else
+    echo "$domain is not a valid domain."
+    exit 1
+fi
+
+# Get IP address and run nmap to get open ports.
+# Uncomment nmap if open ports are needed.
+# #############################################
+echo -e "${GREEN} [+]${RED} Run dig for IP and nmap for ports...${RESET}"
+dig +short $domain > "$nmap/ip_address.txt"
+ip=$(dig +short $domain | head -n 1)
+echo "IP address is: ${ip}"
+#nmap -p- --min-rate 10000 -oA "$nmap/open" $ip
 
 # Run whois.
 # #############################################
 echo -e "${GREEN} [+]${RED} Check whois...${RESET}"
-whois $domain > "$info_path/whois.txt"
+whois $domain > "$information/whois.txt"
 
 # Run subfinder or sublist3r below.
 # Example: subfinder -d domain.com > domain.com/subdomains/subfinder_domains.txt
 # #############################################
 echo -e "${GREEN} [+]${RED} Run subfinder...${RESET}"
-subfinder -d $domain > "$subdomain_path/found.txt"
+subfinder -d $domain > "$subdomains/found.txt"
 
 # Run sublist3r and remove characters from output.
 # The sed commands may need adjustments if sublist3r output changes.
@@ -45,11 +63,11 @@ subfinder -d $domain > "$subdomain_path/found.txt"
 # #############################################
 #echo -e "${GREEN} [+]${RED} Run sublist3r...${RESET}"
 #dom=".${domain}"
-#sublist3r -d $domain > $subdomain_path/sublist3r_results.txt
-#sed -n '/\'"$dom"'/p' "$subdomain_path/sublist3r_results.txt" | tee "$subdomain_path/sublist3r_domains.txt" >/dev/null
-#sed -i 's/....$//' "$subdomain_path/sublist3r_domains.txt"
-#sed -i 's/^.....//' "$subdomain_path/sublist3r_domains.txt"
-#cp -a "$subdomain_path/sublist3r_domains.txt" "$subdomain_path/found.txt"
+#sublist3r -d $domain > $subdomains/sublist3r_results.txt
+#sed -n '/\'"$dom"'/p' "$subdomains/sublist3r_results.txt" | tee "$subdomains/sublist3r_domains.txt" >/dev/null
+#sed -i 's/....$//' "$subdomains/sublist3r_domains.txt"
+#sed -i 's/^.....//' "$subdomains/sublist3r_domains.txt"
+#cp -a "$subdomains/sublist3r_domains.txt" "$subdomains/found.txt"
 
 # Run bbot
 # dir - Get and save newest directory and subdomains scan bbot created.
@@ -58,28 +76,27 @@ subfinder -d $domain > "$subdomain_path/found.txt"
 #echo -e "${GREEN} [+]${RED} Run bbot...${RESET}"
 #bbot -s -t $domain -f subdomain-enum -rf passive
 #dir=$(ls -td $HOME/.bbot/scans/*/ | head -1)
-#cp -a $dir/subdomains.txt $subdomain_path/bbot_domains.txt
-#cp -a $dir/output.txt $subdomain_path/bbot_output.txt
+#cp -a $dir/subdomains.txt $subdomains/bbot_domains.txt
+#cp -a $dir/output.txt $subdomains/bbot_output.txt
 
 # Run assetfinder.
 # Example: assetfinder domain.com | grep domain.com > domain.com/subdomains/assetfinder_found.txt
 # #############################################
 echo -e "${GREEN} [+]${RED} Run assetfinder...${RESET}"
-assetfinder $domain | grep $domain >> "$subdomain_path/found.txt"
+assetfinder $domain | grep $domain >> "$subdomains/found.txt"
 
 # Run amass.
 # It can take a long time to run so uncomment if you want to run it.
 # #############################################
 #echo -e "${GREEN} [+]${RED} Run Amass. This could take a while...${RESET}"
-#amass enum -d $domain >> "$subdomain_path/found.txt"
+#amass enum -d $domain >> "$subdomains/found.txt"
 
 # Run httprobe or cat the output of just subdomains to httprobe.
 # #############################################
 echo -e "${GREEN} [+]${RED} Run httprobe, see what's accessible...${RESET}"
-cat "$subdomain_path/found.txt" | grep $domain | sort -u | httprobe -prefer-https | grep https | sed 's/https\?:\/\///' | tee -a "$subdomain_path/alive.txt"
+cat "$subdomains/found.txt" | grep $domain | sort -u | httprobe -prefer-https | grep https | sed 's/https\?:\/\///' | tee -a "$subdomains/alive.txt"
 
 # Run gowitness using httprobe output.
 # #############################################
 echo -e "${GREEN} [+]${RED} Run gowitness, taking screenshots...${RESET}"
-gowitness file -f "$subdomain_path/alive.txt" -P "$screenshot_path/" --no-http
-
+gowitness file -f "$subdomains/alive.txt" -P "$screenshots/" --no-http
