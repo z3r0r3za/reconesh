4277dd1d3ac46db75c9352c4ec9edcd77d9678da
Pierce Nylon <piercenylon@gmail.com>
2024-03-31 09:25:41 -0700

    Get reponse code to check if domain is up. Add github URLs, short description at the top. Rename vars and the file. Nothing else.

--- recone.sh
+++ recone.sh
@@ -1,10 +1,24 @@
 #!/bin/bash
 
-# Check if first argument exists and print usage.
+# recone.sh - Some basic automated scanning.
+# ##############################################
+# Some need to be uncommented if you want to use
+# them. Tools on github used in this script:
+# nmap (open ports), whois, subfinder, sublist3r, 
+# bbot, assetfinder, amass, httprobe, gowitness.
+
+# Store 200 header status code as true or false.
+up=$(wget --spider --server-response $1 2>&1 | grep '200\ OK' | wc -l)
+# Does first argument exist? If not, print usage and exit.
+# Is the domain up? If not, inform user, print usage and exit.
 if [ $# -eq 0 ]; then
- echo "A domain wasn't specified."
- echo "Usage: recon.sh domain.com"
- exit 1
+    echo "A domain wasn't specified."
+    echo "Usage: recon.sh domain.com"
+    exit 1
+elif [ "$up" = 0 ]; then
+    echo "That domain is down or doesn't exist. "
+    echo "Usage: recon.sh domain.com"
+    exit 1
 fi
 
 # Get first argument, the domain and save it.
@@ -13,18 +27,18 @@ domain=$1
 RED="\033[1;31m"
 GREEN="\033[1;32m"
 RESET="\033[0m"
-# Set up directories.
-base_dir="${domain}_$(date +'%Y%m%dT%H%M%S')"
-nmap="$base_dir/nmap"
-information="$base_dir/info"
-subdomains="$base_dir/subdomains"
-screenshots="$base_dir/screenshots"
+# Set up directories. Add timestamp to base directory. 
+base_directory="${domain}_$(date +'%Y%m%dT%H%M%S')"
+nmap="$base_directory/nmap"
+information="$base_directory/info"
+subdomains="$base_directory/subdomains"
+screenshots="$base_directory/screenshots"
 validate_domain="^([a-zA-Z0-9][a-zA-Z0-9-]{0,61}[a-zA-Z0-9]\.)+[a-zA-Z]{2,}$"
 
 # Check if domain is valid.
 if [[ "$domain" =~ $validate_domain ]]; then
     # Create directories for domain.
-    echo "Creating directories..."
+    echo "Creating directories for the domain..."
     for path in "$nmap" "$information" "$subdomains" "$screenshots"; do
         if [ ! -d "$path" ]; then
             mkdir -p "$path"
@@ -51,12 +65,15 @@ echo -e "${GREEN} [+]${RED} Check whois...${RESET}"
 whois $domain > "$information/whois.txt"
 
 # Run subfinder or sublist3r below.
+# https://github.com/projectdiscovery/subfinder
 # Example: subfinder -d domain.com > domain.com/subdomains/subfinder_domains.txt
 # #############################################
 echo -e "${GREEN} [+]${RED} Run subfinder...${RESET}"
 subfinder -d $domain > "$subdomains/found.txt"
 
 # Run sublist3r and remove characters from output.
+# ORIGINAL: https://github.com/aboul3la/Sublist3r
+# NEWER FORK: https://github.com/RoninNakomoto/Sublist3r2
 # The sed commands may need adjustments if sublist3r output changes.
 # subfinder might find more. sublist3r hasn't been finding every subdomain.
 # Example: sublist3r -d domain.com > domain.com/subdomains/sublist3r_results.txt
@@ -70,6 +87,7 @@ subfinder -d $domain > "$subdomains/found.txt"
 #cp -a "$subdomains/sublist3r_domains.txt" "$subdomains/found.txt"
 
 # Run bbot
+# https://github.com/blacklanternsecurity/bbot
 # dir - Get and save newest directory and subdomains scan bbot created.
 # May not work if your user is root, or might ask for password. Not tested.
 # #############################################
@@ -80,23 +98,27 @@ subfinder -d $domain > "$subdomains/found.txt"
 #cp -a $dir/output.txt $subdomains/bbot_output.txt
 
 # Run assetfinder.
+# https://github.com/tomnomnom/assetfinder
 # Example: assetfinder domain.com | grep domain.com > domain.com/subdomains/assetfinder_found.txt
 # #############################################
 echo -e "${GREEN} [+]${RED} Run assetfinder...${RESET}"
 assetfinder $domain | grep $domain >> "$subdomains/found.txt"
 
 # Run amass.
+# https://github.com/owasp-amass/amass
 # It can take a long time to run so uncomment if you want to run it.
 # #############################################
 #echo -e "${GREEN} [+]${RED} Run Amass. This could take a while...${RESET}"
 #amass enum -d $domain >> "$subdomains/found.txt"
 
 # Run httprobe or cat the output of just subdomains to httprobe.
+# https://github.com/tomnomnom/httprobe
 # #############################################
 echo -e "${GREEN} [+]${RED} Run httprobe, see what's accessible...${RESET}"
 cat "$subdomains/found.txt" | grep $domain | sort -u | httprobe -prefer-https | grep https | sed 's/https\?:\/\///' | tee -a "$subdomains/alive.txt"
 
 # Run gowitness using httprobe output.
+# https://github.com/sensepost/gowitness
 # #############################################
 echo -e "${GREEN} [+]${RED} Run gowitness, taking screenshots...${RESET}"
 gowitness file -f "$subdomains/alive.txt" -P "$screenshots/" --no-http
